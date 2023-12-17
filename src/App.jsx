c5fede014d2326f1943d7f9f4a762a634d01f9ef
Kaushal <Kausraj11@gmail.com>
2023-12-17 20:06:31 +0530

    Added Sample component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-9xl '>Hello World</h1>
+      <FontAwesomeIcon icon={faHouse} />
+    </>
+  )
+}
+
+export default App
